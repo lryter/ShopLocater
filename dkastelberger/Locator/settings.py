2ba40b8fd67ad341cb933a7ac00c0bab6909efd6
Dominik Castelberg <Dominik Castelberg>
2016-05-18 11:44:27 +0200

    fixed STATIC_ROOT path

--- dkastelberger/Locator/settings.py
+++ dkastelberger/Locator/settings.py
@@ -107,4 +107,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/ShopLocater/templates/index/' ######
-STATIC_ROOT = "/LocatorEnv/LocatorProject/Locater/dkastelberger/static/" ######
+STATIC_ROOT = "~/LocatorEnv/LocatorProject/Locater/dkastelberger/static/" ######
