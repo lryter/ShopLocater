a9d8ddf30a89b7c7c62c9db5cc69cdd1e87d2ee6
Lucien Ryter <lryter@crosstales.com>
2016-05-30 12:09:16 +0200

    Json changes

--- lryter/Locator/urls.py
+++ lryter/Locator/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import include, url,  patterns
 from django.contrib import admin
 from rest_framework import routers,  serializers,  viewsets
-from ShopLocater.views import TaskViewSet
+from ShopLocater.views import TaskViewSet,  UserViewSet
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 router = routers.DefaultRouter()
 router.register(r'task',  TaskViewSet)
-router.register(r'user',  TaskViewSet)
+router.register(r'user',  UserViewSet)
 
 urlpatterns = [
     url(r'^',  include('ShopLocater.urls')), 
