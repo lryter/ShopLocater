84366a940c0920c1e6f0770d775b3dd108665743
Lucien Ryter <lryter@crosstales.com>
2016-06-01 10:34:45 +0200

    Fixed google map issue

--- lryter/static/js/my.js
+++ lryter/static/js/my.js
@@ -1,8 +1,7 @@
 function HideForAndroid(){
 
 var Hide = document.getElementsByClassName("hideit");
-//HideNav.className += ' hide';
-    
+
     for(var i = 0; i<Hide.length; i++){
         Hide[i].className += ' hide';
     }
